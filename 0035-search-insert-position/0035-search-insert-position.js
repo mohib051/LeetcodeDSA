b0badbd6b2a1f422813267aa632ae8827c6f21e3
Kazi Mohibuzzaman <83270382+mohib051@users.noreply.github.com>
2025-02-13 12:19:53 +0530

    Time: 0 ms (100%), Space: 49.3 MB (32.87%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let l = 0 , r = nums.length-1
+     while(l<=r){
+     let mid= Math.floor((l+r)/2)
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] < target) {
+            l = mid +1;
+        }
+        else  r = mid -1;
+     }
+     return l ;
+};
