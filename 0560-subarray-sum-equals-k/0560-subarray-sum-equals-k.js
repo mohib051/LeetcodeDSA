93a3e2e4e6bc972c526f047d1c78c52f9b99646d
Kazi Mohibuzzaman <83270382+mohib051@users.noreply.github.com>
2025-02-20 12:09:46 +0530

    Time: 14 ms (88.25%), Space: 63.2 MB (5.23%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function(nums, k) {
+    let map = new Map()
+    let sum = 0 ,ans=0
+    map.set(sum,1);
+    for(let i =0; i<nums.length; i++){
+        sum += nums[i];
+        if(map.has(sum-k)) ans += map.get(sum-k)
+
+        map.set(sum,(map.get(sum)||0)+1)
+    }
+    return ans 
+};
